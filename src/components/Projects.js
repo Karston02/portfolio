0144f4d8149664b74d65c81de28b5d42be6d78c5
Karston Kuciemba <kuciembakarston@gmail.com>
2023-05-04 22:30:08 -0500

    Added projects section

---
+++ src/components/Projects.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Projects.css'
+
+function Projects() {
+  return (
+    <section className="projects-container">
+        <div></div>
+    </section>
+  )
+}
+
+export default Projects
