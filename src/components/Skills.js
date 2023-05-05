1cbe0f00d3415300dc2fea47e99689311cf51dd3
Karston Kuciemba <kuciembakarston@gmail.com>
2023-05-04 22:29:40 -0500

    Added skills section

---
+++ src/components/Skills.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Skills.css'
+
+function Skills() {
+  return (
+    <section className="skills-container">
+        <div></div>
+    </section>
+  )
+}
+
+export default Skills
