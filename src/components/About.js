434baceec6eb8adb8a1cd75d2b55e5a0f21f50fa
Karston Kuciemba <kuciembakarston@gmail.com>
2023-05-07 13:17:57 -0500

    Completed 3D sphere for about

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./About.css";
 import moonMan from "../images/moon-man.png";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Sphere, MeshDistortMaterial } from "@react-three/drei";
 
 const aboutMe =
   "Hello! I'm Karston Kuciemba. I have a passion for programming and problem-solving! I'm currently studying Computer Science and Economics at the University of Texas at Austin. I have always been fascinated with how technology can be used to improve people's lives. I am currently looking for Software or Web Development Internships!";
-const beyondCode = "Aside from programming, I enjoy spending my free time with my family and friends. I am also an avid Liverpool supporter and love to watch soccer as often as I can! I have played soccer my entire life. I am also a passionate cook... though my burnt food says I have much to learn."
+const beyondCode =
+  "Aside from programming, I enjoy spending my free time with my family and friends. I am also an avid Liverpool supporter and love to watch soccer as often as I can! I have played soccer my entire life. I am also a passionate cook... though my burnt food says I have much to learn.";
 function About() {
   return (
     <section className="about-container">
@@ -16,7 +19,20 @@ function About() {
           <p>{beyondCode}</p>
         </div>
         <div className="aboutme-flex aboutme-design">
-          <img src={moonMan}></img>
+          <Canvas className="stack" camera={{ fov: 25, position: [5, 5, 5] }}>
+            <OrbitControls enableZoom={false} />
+            <ambientLight intensity={1} />
+            <directionalLight position={[7, 2, 3]}/>
+            <Sphere args={[1, 100, 200]} scale={1.4}>
+              <MeshDistortMaterial
+                color="#B629CF"
+                attach="material"
+                distort={0.5}
+                speed={2.5}
+              />
+            </Sphere>
+          </Canvas>
+          <img className="stack" src={moonMan} />
         </div>
       </div>
     </section>
